bc4f974577308e06d5aabaad96e9bf6828001732
henriquecscode <hcs_games@hotmail.com>
2018-09-05 20:48:05 +0100

    Does not check if the user already has the role

--- commands/mod/mute.js
+++ commands/mod/mute.js
@@ -28,43 +28,36 @@ class MuteCommand extends Command {
         })
     }
 
-    async run(message, { muser , data}) {
+    async run(message, { muser, data }) {
         if (!muser.hasPermission('MANAGE_MESSAGES')) {
 
-            if (!muser.roles.has(configs.muteroleid)) {
+            let mutesettings = delay.DelaySettings(data); //Uses the data to set the time, the time unit and the reason
 
-                let mutesettings = delay.DelaySettings(data); //Uses the data to set the time, the time unit and the reason
-                
-                let status = notifications.AddMute(muser.id, mutesettings[0], mutesettings[1], mutesettings[2]);
-                let durationreason = delay.DurationReason(mutesettings[0], mutesettings[1], mutesettings[2]);
+            let status = notifications.AddMute(muser.id, mutesettings[0], mutesettings[1], mutesettings[2]);
+            let durationreason = delay.DurationReason(mutesettings[0], mutesettings[1], mutesettings[2]);
 
-                if (status === 'duplicated') { //Already asked for that mute status
-                    message.channel.send(`${message.member}, that user already has that mute.\nDo p$unmute to remove it`);
+            if (status === 'duplicated') { //Already asked for that mute status
+                message.channel.send(`${message.member}, that user already has that mute.\nDo p$unmute to remove it`);
+            }
+            else { //There is going to be a new delay set
+                if (status === 'updated') { //Updates the mute status and resets the time to remove the role
+                    message.channel.send(`${message.member}, his mute has been updated\n${durationreason}`);
                 }
-                else { //There is going to be a new delay set
-                    if (status === 'updated') { //Updates the mute status and resets the time to remove the role
-                        message.channel.send(`${message.member}, his mute has been updated\n${durationreason}`);
-                    }
-                    else { //Creates the mute status
-                        message.channel.send(`${message.member}, the user has been muted\n${durationreason}`);
-                        muser.addRole(configs.muteroleid);
-                    }
-                    console.log("Mute detected");
-                    if(mutesettings[0]){ //There was a delay time specified
-                        await delay.Delay(mutesettings[0], mutesettings[1]);
+                else { //Creates the mute status
+                    message.channel.send(`${message.member}, the user has been muted\n${durationreason}`);
+                    muser.addRole(configs.muteroleid);
+                }
+                console.log("Mute detected");
+                if (mutesettings[0]) { //There was a delay time specified
+                    await delay.Delay(mutesettings[0], mutesettings[1]);
 
-                        let datasettings = notifications.ToMute(muser.id);
+                    let datasettings = notifications.ToMute(muser.id);
 
-                        if(mutesettings[0] === datasettings[0] && mutesettings[1] === datasettings[1] && mutesettings[2] === datasettings[2]){
+                    if (mutesettings[0] === datasettings[0] && mutesettings[1] === datasettings[1] && mutesettings[2] === datasettings[2]) {
                         notifications.RemoveMute(muser.id);
                         muser.removeRole(configs.muteroleid);
-                        }
                     }
                 }
-
-            }
-            else {
-                return message.channel.send(`${message.member}, that user is already muted`);
             }
 
         }
